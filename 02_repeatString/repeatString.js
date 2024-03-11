e98a17cf13823bd80381f20c8c5d4f397186af02
Kykneon <158997169+Kykneon@users.noreply.github.com>
2024-03-11 17:19:05 +0200

    Completes excercise 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+    let isString = '';
+    let numSign = Math.sign(num);
 
+    if (numSign === -1) {
+        isString = "ERROR";
+    } else {
+        for (let i = 0; i < num; i++) {
+            isString += string;
+        }
+    }
+
+    return isString;
 };
 
 // Do not edit below this line
