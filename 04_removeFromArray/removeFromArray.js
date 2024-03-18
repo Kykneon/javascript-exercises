d44b6aece76f50e2046ab5b72fa65afd4ef8a17f
Kykneon <158997169+Kykneon@users.noreply.github.com>
2024-03-18 13:45:11 +0200

    Completes exercise 04_removerFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
 const removeFromArray = function() {
-
+    let modifiedArray = arguments[0];
+    for (let i = 1; i <= arguments.length - 1; i++) {
+        modifiedArray = modifiedArray.filter((element) => element !== arguments[i])
+    }
+    return modifiedArray;
 };
 
 // Do not edit below this line
