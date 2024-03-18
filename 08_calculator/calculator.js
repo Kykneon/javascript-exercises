0cfc5d09bdecb3a9454034b80426aa943a2ac306
Kykneon <158997169+Kykneon@users.noreply.github.com>
2024-03-18 17:28:26 +0200

    Completes exercise 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  let sum = 0;
+  for (let int of array) {
+    sum += int;
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  let sum = array[0];
+  for (i = 1; i <= array.length - 1; i++) {
+    sum = sum * array[i];
+  }
+  return sum;
 };
 
-const power = function() {
-	
+const power = function (val, pow) {
+  let result = val;
+  for (i = 1; i < pow; i++) {
+    result = result * val;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  let result = num;
+  if (num === 0) {
+    return 1;
+  } else {
+    for (i = num - 1; i > 0; i--) {
+      result = result * i;
+    }
+    return result;
+  }
 };
 
 // Do not edit below this line
