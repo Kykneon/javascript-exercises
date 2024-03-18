28abb61374a92b090597d9e3049780fabc39c928
Kykneon <158997169+Kykneon@users.noreply.github.com>
2024-03-18 16:51:20 +0200

    Completes exercise 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+  let converted;
+  let rounded;
+  converted = (temp - 32) * 5/9;
+  rounded = converted.toFixed(1);
+  return parseFloat(rounded)
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  let converted;
+  let rounded;
+  converted = temp * (9 / 5) + 32;
+  rounded = converted.toFixed(1);
+  return parseFloat(rounded)
 };
 
 // Do not edit below this line
