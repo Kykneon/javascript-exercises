9659760fdcf8a9698cc6602f9b35642c3fd13d75
Kykneon <158997169+Kykneon@users.noreply.github.com>
2024-03-19 12:31:15 +0200

    Completes exercise 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+    let string = input.toLowerCase().replaceAll(/[\.,?!\s]/g, '')
+    let reversedString = string.split('').reverse().join('');
+    if (string === reversedString) return true; 
+    else return false;
 };
 
 // Do not edit below this line
