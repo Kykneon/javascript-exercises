346b9874b25a5270bda9a9e501d0e4796904ada5
Kykneon <158997169+Kykneon@users.noreply.github.com>
2024-03-20 09:33:41 +0200

    Completes exercise 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function (input) {
+    let cleanedInput = +input;
+    let fibPrevious = 0;
+    let fibCurrent = 1;
+    let sum = 1;
+    if (cleanedInput === 0) {
+        return 0;
+    } else if (Math.sign(cleanedInput) === -1) {
+        return "OOPS";
+    } else {
+        for (i = 1; i < cleanedInput; i++) {
+            sum = fibPrevious + fibCurrent;
+            fibPrevious = fibCurrent;
+            fibCurrent = sum;
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
