158f6c46f919c393f8a4a725c6f7cae2b0520a05
Kykneon <158997169+Kykneon@users.noreply.github.com>
2024-03-21 13:41:55 +0200

    Completes exercise 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+    const date = new Date();
+    let currentYear = date.getFullYear();
+    let personAge = 0;
 
+    for (let person of people) {
+        if (person.yearOfDeath) {
+            person.age = person.yearOfDeath - person.yearOfBirth;
+        } else {
+            person.age = currentYear - person.yearOfBirth;
+        }
+    }
+    for (let person of people) if (person.age > personAge) personAge = person.age;
+    for (let person of people) if (person.age === personAge) return person;
 };
 
 // Do not edit below this line
 module.exports = findTheOldest;
+
+
+// Iterate through object
+// function to compare year of death and year of death to determine age
+// save age as key:value in refereenced object element
+// function (method?) to select object with the highest age and return its name value
